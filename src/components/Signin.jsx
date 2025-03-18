4b698be88e9e33a7a127277269b628fccf4cba59
vicky-axios <vigneshkumarpmcv@gmail.com>
2025-03-18 12:45:39 +0530

    cmt one changes

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,16 +1,29 @@
 /* import React from 'react' */
-
+import { IoMdContact } from "react-icons/io";
 function Signin() {
     return (
-        <div className="w-full h-screen flex items-center justify-center">
-            <div className="w-[90%] max-w-sm md:max-w-md lg:max-w-md p-5 bg-blue-500 flex-col flex items-center gap-3 rounded-xl ">
-                <h1 className="text-lg md:text-xl font-semibold">Create Account</h1>
+        <div className="w-full h-screen flex items-center justify-center bg-black">
 
-                <p className="text-xs md:text-sm text-gray-500 text-center">
-                    <span className="text-white">Sign up</span>
-                </p>
+            <div className="w-[90%] h-min  max-w-sm md:max-w-md lg:max-w-md p-5 bg-blue-500 flex-col flex items-center gap-7 top-0 rounded-xl ">
+                <div className="text-5xl m-5  justify-items-center">
+                    <IoMdContact className="" />
+                </div>
+                <div className="text-lg md:text-xl font-semibold ">
+                    <h1 >Create Account</h1>
+                </div>
 
-                <div className="w-full flex flex-col gap-3">
+
+
+
+                <div className="w-full flex flex-col gap-7">
+                    <div className='w-full flex items-center gap-2  bg-gray-800 p-2 rounded-xl'>
+                        <input
+                            type="text"
+                            placeholder="Name"
+                            className="bg-transparent border-0 w-full outline-none text-sm md:text-base"
+                        />
+                    </div>
+
                     <div className='w-full flex items-center gap-2 bg-gray-800 p-2 rounded-xl'>
                         <input
                             type="email"
@@ -27,9 +40,12 @@ function Signin() {
                         />
                     </div>
                 </div>
-                <button className='w-fll p-2 bg-white rounded-xl mt-5 hover:bg-blue-300 text-sm'>
-                    Login
-                </button>
+                <div className="">
+                    <button className=' p-2 bg-white rounded-xl mt-5  text-base '>
+                        Create
+                    </button>
+                </div>
+
             </div>
 
         </div>
